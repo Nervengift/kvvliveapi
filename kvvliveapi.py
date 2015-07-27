fc1718a0ffd1769771780b728180534d245fbfa5
Clemens Wallrath <dev@nervengiftlabs.de>
2015-07-27 18:32:45 +0200

    Departure.time now is a datetime.datetime
    the new attribute .timestr now contains the string version

--- kvvliveapi.py
+++ kvvliveapi.py
@@ -7,7 +7,8 @@ try:
 except ImportError:
     import urllib2 as _urllib
     from urllib import quote_plus,urlencode
-
+from datetime import datetime,timedelta
+import re
 import json
 import re
 import sys
@@ -32,10 +33,38 @@ class Departure:
         self.route = route
         self.destination = destination
         self.direction = direction
-        self.time = time #TODO: to timestamp?
+        self.timestr = time #TODO: to timestamp?
         self.lowfloor = lowfloor
         self.realtime = realtime
         self.traction = traction
+        self.time = self._str_to_time(time)
+
+    def _str_to_time(self, timestr):
+        """ _str_to_time converts a time string as given in the API response to da datetime.datetime """
+        time = datetime.now()
+
+        # "0" ("sofort")
+        if timestr == "sofort":
+            return time
+
+        # "5 min"
+        re_min = re.compile("^([1-9]) min$")
+        match = re_min.match(timestr)
+        if match:
+            time += timedelta(minutes=int(match.group(1)))
+            return time
+
+        # 14:23
+        re_time = re.compile("^([0-2]?[0-9]):([0-5][0-9])$")
+        match = re_time.match(timestr)
+        if match:
+            hours = int(match.group(1))
+            mins = int(match.group(2))
+            time_new = time.replace(hour=hours, minute=mins)
+            if time_new < time:
+                time_new += timedelta(days=1)
+            time = time_new
+            return time
 
     @staticmethod
     def from_json(json):
@@ -45,7 +74,7 @@ class Departure:
         return Departure(json["route"], json["destination"], json["direction"], time, json["lowfloor"], json["realtime"], json["traction"])
 
     def pretty_format(self):
-        return self.time + ("  " if self.realtime else "* ") + (" " if self.time != "sofort" else "") + self.route + " " + self.destination
+        return self.timestr + ("  " if self.realtime else "* ") + (" " if self.timestr != "sofort" else "") + self.route + " " + self.destination
 
 
 def _query(path, params = {}):

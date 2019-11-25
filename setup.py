fa446d10305332318ab5aad3e818459daed899ea
loelkes <christian.loelkes@gmail.com>
2019-11-25 11:07:32 +0100

    use doctop for cli argument parsing

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ import setuptools
 
 setuptools.setup(
     name='kvvliveapi',
-    version='0.1.3',
+    version='0.1.4',
     packages=setuptools.find_packages(),
     author='Nervengift',
     author_email='github@nervengiftlabs.de',
     description='KVV live API bindings',
     keywords='kvv api',
-    url='https://github.com/Nervengift/kvvliveapi'
+    url='https://github.com/Nervengift/kvvliveapi',
+    install_requires=['docopt']
 )

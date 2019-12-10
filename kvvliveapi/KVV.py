c8c5cb725be79d9e52c1e592acc86b35ad10a7e1
Christian Loelkes <christian.loelkes@gmail.com>
2019-12-10 20:31:51 +0100

    code refactoring

--- kvvliveapi/KVV.py
+++ kvvliveapi/KVV.py
@@ -7,14 +7,13 @@ try:
 except ImportError:
     import urllib2 as _urllib
     from urllib import quote_plus,urlencode
-from datetime import datetime,timedelta
 import re
 import json
-import re
-import sys
+from datetime import datetime, timedelta
 
 API_KEY = "377d840e54b59adbe53608ba1aad70e8"
 API_BASE = "https://live.kvv.de/webapp/"
+
 
 class Stop:
     def __init__(self, name, stop_id, lat, lon):
@@ -28,9 +27,7 @@ class Stop:
         return Stop(json["name"], json["id"], json["lat"], json["lon"])
 
     def __repr__(self):
-        return "Stop(name={}, stop_id={}, lat={}, lon={})".format(
-            self.name, self.stop_id, self.lat, self.lon
-        )
+        return "Stop(name={0.name}, stop_id={0.stop_id}, lat={0.lat}, lon={0.lon})".format(self)
 
 
 class Departure:
@@ -38,7 +35,7 @@ class Departure:
         self.route = route
         self.destination = destination
         self.direction = direction
-        self.timestr = time #TODO: to timestamp?
+        self.timestr = time  # TODO: to timestamp?
         self.lowfloor = lowfloor
         self.realtime = realtime
         self.traction = traction
@@ -74,17 +71,19 @@ class Departure:
 
     @staticmethod
     def from_json(json):
-        time = json["time"]
-        if time == "0":
-            time = "sofort"
+        time = "sofort" if json["time"] == "0" else json["time"]
 
         # stopPosition either has been banned from json or is not defined for all stops
-        if "stopPosition" in json:
-            stopPosition = json["stopPosition"]
-        else:
-            stopPosition = None
+        stopPosition = json.get("stopPosition", None)
 
-        return Departure(json["route"], json["destination"], json["direction"], time, json["lowfloor"], json["realtime"], json["traction"], stopPosition)
+        return Departure(json["route"],
+                         json["destination"],
+                         json["direction"],
+                         time,
+                         json["lowfloor"],
+                         json["realtime"],
+                         json["traction"],
+                         stopPosition)
 
     def pretty_format(self, alwaysrelative=False):
         if alwaysrelative and self.timestr != "sofort":
@@ -92,14 +91,18 @@ class Departure:
             timestr = "{:>3} min".format(mins)
         else:
             timestr = self.timestr
-        return timestr + ("  " if self.realtime else "* ") + (" " if timestr != "sofort" else "") + self.route + " " + self.destination
+
+        output = timestr + "  " if self.realtime else "* "
+        output += " " if timestr != "sofort" else ""
+
+        return "{0}{1.route} {1.destination}".format(output, self)
 
     def __repr__(self):
         return "Departure(route={}, destination={}, direction={}, time={})"\
             .format(self.route, self.destination, self.direction, self.time)
 
 
-def _query(path, params = {}):
+def _query(path, params={}):
     params["key"] = API_KEY
     url = API_BASE + path + "?" + urlencode(params)
     req = _urllib.Request(url)
@@ -119,38 +122,34 @@ def _query(path, params = {}):
     return json.loads(handle.read().decode("utf8"))
 
 def _search(query):
-    json = _query(query)
-    stops = []
-    if json:
-        for stop in json["stops"]:
-            stops.append(Stop.from_json(stop))
-    return stops
+    json = _query(query) or []
+    return [Stop.from_json(stop) for stop in json["stops"]]
+
 
 def search_by_name(name):
     """ Search for stops by name
         returns a list of Stop objects
     """
-    return _search("stops/byname/" + quote_plus(name))
+    return _search("stops/byname/" + requests.utils.requote_uri(name))
+
 
 def search_by_latlon(lat, lon):
     """ Search for stops by latitude and longitude
         returns a list of Stop objectss
     """
-    return _search("stops/bylatlon/" + lat + "/" + lon)
+    return _search("stops/bylatlon/{}/{}".format(lat, lon))
+
 
 def search_by_stop_id(stop_id):
     """ Search for a stop by its stop_id
         returns a list that should contain only one stop
     """
-    return [Stop.from_json(_query("stops/bystop/" + stop_id))]
+    return [Stop.from_json(_query("stops/bystop/{}".format(stop_id)))]
+
 
 def _get_departures(query, max_info=10):
-    json = _query(query, {"maxInfos" : str(max_info)})
-    departures = []
-    if json:
-        for dep in json["departures"]:
-            departures.append(Departure.from_json(dep))
-    return departures
+    json = _query(query, {"maxInfos": str(max_info)}) or []
+    return [Departure.from_json(dep) for dep in json["departures"]]
 
 
 def get_departures(stop_id, max_info=10):
@@ -160,10 +159,12 @@ def get_departures(stop_id, max_info=10):
     return _get_departures("departures/bystop/" + stop_id, max_info)
 
 def get_departures_by_route(stop_id, route, max_info=10):
+
     """ Return a list of Departure objects for a given stop stop_id and route
         optionally set the maximum number of entries
     """
-    return _get_departures("departures/byroute/" + route + "/" + stop_id, max_info)
+    return _get_departures("departures/byroute/{}/{}".format(route, stop_id), max_info)
+
 
 def errorstring(e):
     if hasattr(e, "code"):
